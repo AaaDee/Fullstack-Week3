2486d79b3e1b9d47429573271c052a05081df0ad
Aarne Rissanen <ari.outermedia.de>
2022-10-27 02:15:36 +0200

    remove file specs

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'e2e/*.cy.js',
-    supportFile: false,
     pageLoadTimeout: 120000
   }
 })
