57c1bb5c26e3cc646c9c77ee5c06f58cfe1ce0bc
Aarne Rissanen <ari.outermedia.de>
2022-10-27 02:01:20 +0200

    add cypress timeout

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // eslint-disable-next-line no-unused-vars
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    specPattern: 'e2e/*.cy.js',
+    supportFile: false,
+    pageLoadTimeout: 120000
+  }
 })
